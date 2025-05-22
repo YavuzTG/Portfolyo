f47c1aa8705d1aece3b2ce16b2a680f81a1a29d8
YavuzTG <yavuzturker@icloud.com>
2025-05-22 20:03:46 +0300

    Son değişiklikler eklendi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
     <footer className="footer">
       <p>Sosyal Medya:</p>
       <div className="social-links">
-        <a href="https://github.com/kullaniciadi" target="_blank" rel="noreferrer">GitHub</a>
-        <a href="https://x.com/kullaniciadi" target="_blank" rel="noreferrer">X</a>
+        <a href="https://github.com/YavuzTG" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://x.com/YavuzGurleyuk" target="_blank" rel="noreferrer">X</a>
         <a href="https://facebook.com/kullaniciadi" target="_blank" rel="noreferrer">Facebook</a>
       </div>
     </footer>
